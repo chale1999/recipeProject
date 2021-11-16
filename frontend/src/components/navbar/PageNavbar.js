97de4a4c6ba50355685322c9f0a68547a5bd32f8
Christopher Rumpf <crumpf05@gmail.com>
2021-11-16 13:53:12 -0500

    more style changes to navbar

--- frontend/src/components/navbar/PageNavbar.js
+++ frontend/src/components/navbar/PageNavbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './PageNavbar.css';
-import { Search, Notifications, AccountCircle} from '@mui/icons-material';
+import { Bookmark, FoodBank, Search, Notifications, AccountCircle} from '@mui/icons-material';
 import pfp from '../imgs/person.jpg';
 function PageNavbar()
 {   
@@ -16,7 +16,7 @@ function PageNavbar()
     <div id="navbar">
         <div class="navbar__container">
             <div id="logoContainer">
-                <span id="navbar__logo">Recipe!</span>
+                <span id="navbar__logo">MegaBites</span>
             </div>
 
             <div class="navbar__toggle" id="mobile-menu">
@@ -31,9 +31,12 @@ function PageNavbar()
             </div>
 
             <div id="navBarRight">
-                <span class="navLink">Feed</span>
-                <span class="navLink">Bookmarks</span>
-                <span class="navLink">My Profile</span>
+                <div class="navItem">
+                    <FoodBank fontSize="large"/>
+                </div>
+                <div class="navItem">
+                    <Bookmark fontSize="large"/> 
+                </div>
                 <div class="navItem">
                   <Notifications fontSize="large"/>
                   <span id="notifCount">1</span>
