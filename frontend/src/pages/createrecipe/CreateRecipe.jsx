f6b752f73b09a0c5120762fbc9bb5791e88ef9ea
Christopher Rumpf <crumpf05@gmail.com>
2021-11-23 10:48:26 -0500

    more create-recipe elements and styling

--- frontend/src/pages/createrecipe/CreateRecipe.jsx
+++ frontend/src/pages/createrecipe/CreateRecipe.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./CreateRecipe.css";
 import PageNavbar from "../../components/navbar/PageNavbar.jsx";
+import imageUpload from "../../components/imgs/uploadbutton.JPG";
 
 
 
@@ -14,53 +15,54 @@ const CreateRecipe = () =>
         <div class="newRecipe">
         <div class="newInfo1">
             <div class="newRecipeImage">
-                <h4><b>Upload Image</b></h4>
-                <form>
-                    <input type="file" id="myfile" accept=".png, .jpg, .jpeg"/>
-                </form>
+                <p class="fieldLabel">Upload Image</p>
+                <label for="recipeImage">
+                    <img id="image" height="200" src={imageUpload}/>
+                </label>
+                <input id="recipeImage" type="file" accept=".png, .jpg, .jpeg"/>
             </div>
             <hr/>
             <div class="newInfo1Row1">
                 <div class="timeField">
-                    <h6>Preparation Time</h6>
+                    <p class="fieldLabel">Preparation Time</p>
                     <input type="text" id="timeInput" placeholder="ex. 30 mins"/>
                 </div>
                 <br/>
                 <div class="timeField">
-                    <h6>Cooking Time</h6>
+                    <p class="fieldLabel">Cooking Time</p>
                     <input type="text" id="timeInput" placeholder="ex. 30 mins"/>
                 </div>
                 <br/>
                 <div class="timeField">
-                    <h6># of Servings</h6>
+                    <p class="fieldLabel">Number of servings</p>
                     <input type="text" id="timeInput" placeholder="ex. 2 servings"/>
                 </div>
             </div>
         </div>
         <div class="newInfo2">
             <div class="newRecipeTitle">
-                    <h4><b>Recipe Name</b></h4>
+                <p class="fieldLabel">Recipe Title</p>
                     <form>
                         <input type="text" id="recipeName" placeholder="Name"/>
                    </form>
                 </div>
             <hr/>
             <div className="newRecipeDesc">
-                <h4><b>Recipe Description</b></h4>
+                <p class="fieldLabel">Recipe Description</p>                
                 <form>
                     <textarea id="recipeDesc" placeholder="Describe your recipe in 150 words!" maxLength="150"/>
                 </form>
             </div>
             <hr/>
             <div className="newRecipeIngr">
-                <h4><b>Ingredients List</b></h4>
+                <p class="fieldLabel">Ingredients List</p>
                 <form>
                     <textarea id="recipeDesc" placeholder="Place each ingredient on a new line."/>   
                 </form>
             </div>
             <hr/>
             <div className="newRecipeProc">
-                <h4><b>Procedure</b></h4>
+                <p class="fieldLabel">Procedure</p>
                 <form>
                     <textarea id="recipeDesc" placeholder="Describe the procedure for creating your recipe! Try to place each step on a new line."/>
                 </form>
