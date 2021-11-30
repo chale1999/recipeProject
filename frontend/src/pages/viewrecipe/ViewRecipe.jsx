bad7b083a9227703c94a0cc54cee49506ad137dd
Christopher Rumpf <crumpf05@gmail.com>
2021-11-30 17:20:51 -0500

    added prototype viewr recipe page

---
+++ frontend/src/pages/viewrecipe/ViewRecipe.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ViewRecipe.css';
+import PageNavbar from '../../components/navbar/PageNavbar.jsx';
+
+const ViewRecipe = () =>{
+    return(
+        <div class="viewRecipePage">
+            <PageNavbar/>
+            <div class="viewRecipeContainer">
+                hello test hello hi
+            </div>
+        </div>
+
+    );
+}
+
+export default ViewRecipe;
