53225ae979ea3abc7e71c006b1a7922af80056cc
Christopher Rumpf <crumpf05@gmail.com>
2021-12-01 21:45:32 -0500

    added elements and styling to profile page

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -1,8 +1,14 @@
-import NavBar from '../../components/navbar/PageNavbar';
+import PageNavbar from '../../components/navbar/PageNavbar';
 import './Profile.css';
 import jwt_decode from "jwt-decode";
 import axios from 'axios';
 import { useHistory } from 'react-router';
+import {Link} from 'react-router-dom';
+import pfp from '../../components/imgs/person.jpg';
+import cover from '../../components/imgs/food.jpg';
+import recipeImage from '../../components/imgs/food.jpg';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 
 const Profile = () =>
@@ -38,20 +44,105 @@ const Profile = () =>
 
 	getProfile();
 	return(
-		<>
-		  <NavBar/>
+		<div class="profileScreen">
+		  <PageNavbar/>
 		  <div className="profile">
 			<div className="profileTop">
-			cover photo and pfp
+				<div className="profileCover">
+					<img src={cover} className="profileCoverImg"/>
+					<img src={pfp} className="profileUserImg"/>
+				</div>
+				<div className="profileInfo">
+					<h4 className="profileInfoName"><b>Sample Name</b></h4>
+					<span className="profileInfoDesc">Hello! My name is Sample Name. I love to cook and specialize in baking!</span>
+				</div>
 			</div>
 			<div className="break">
 
 			</div>
-			<div className="tabs">
-			tabs like followers, my recipes, etc
+			<div class="profileBottom">
+				<div className="profileBottomInfo">
+					<div className="tabs">
+					<Tabs>
+    						<TabList>
+      							<Tab>My Recipes</Tab>
+      							<Tab>Users You Follow</Tab>
+								<Tab>Users Following You</Tab>
+    						</TabList>
+    						<TabPanel>
+      							<div className="feedArea">
+                        			<div className="feedItem">
+                            			<img id="itemImage" alt = "recipe pic" src={recipeImage}></img>
+                            			<div className="itemInfo">
+                                			<span id="itemTitle">This is a feed item!</span>
+                            			</div>
+                        			</div>
+                        			<div className="feedItem">
+                            			<img id="itemImage" alt = "recipe pic" src={recipeImage}></img>
+                            				<div className="itemInfo">
+                                				<span id="itemTitle">This is a feed item!</span>
+                            				</div>
+                        			</div>
+                        			<div className="feedItem">
+                        	    		<img id="itemImage" alt = "recipe pic" src={recipeImage}></img>
+                        				    <div className="itemInfo">
+                            				    <span id="itemTitle">This is a feed item!</span>
+                            				</div>
+                        			</div>
+                    			</div>
+    						</TabPanel>
+    						<TabPanel>
+							<div className="profileFollowedUserContainer">
+								<div className="followedUser">
+									<Link to="/profile"><img src={pfp} alt="Your profile pic" id="profilePicture"/></Link>
+									<span>Sample Name</span>
+								</div>
+								<br/>
+								<div className="followedUser">
+									<Link to="/profile"><img src={pfp} alt="Your profile pic" id="profilePicture"/></Link>
+									<span>Sample Name</span>
+								</div>
+								<br/>
+								<div className="followedUser">
+									<Link to="/profile"><img src={pfp} alt="Your profile pic" id="profilePicture"/></Link>
+									<span>Sample Name</span>
+								</div>
+								<br/>
+								<div className="followedUser">
+									<Link to="/profile"><img src={pfp} alt="Your profile pic" id="profilePicture"/></Link>
+									<span>Sample Name</span>
+								</div>
+							</div>
+    						</TabPanel>
+							<TabPanel>
+							<div className="profileFollowedUserContainer">
+								<div className="followedUser">
+									<Link to="/profile"><img src={pfp} alt="Your profile pic" id="profilePicture"/></Link>
+									<span>Sample Name</span>
+								</div>
+								<br/>
+								<div className="followedUser">
+									<Link to="/profile"><img src={pfp} alt="Your profile pic" id="profilePicture"/></Link>
+									<span>Sample Name</span>
+								</div>
+								<br/>
+								<div className="followedUser">
+									<Link to="/profile"><img src={pfp} alt="Your profile pic" id="profilePicture"/></Link>
+									<span>Sample Name</span>
+								</div>
+								<br/>
+								<div className="followedUser">
+									<Link to="/profile"><img src={pfp} alt="Your profile pic" id="profilePicture"/></Link>
+									<span>Sample Name</span>
+								</div>
+							</div>
+    						</TabPanel>
+  						</Tabs>
+					</div>
+				</div>
 			</div>
 	      </div>
-		</>
+		</div>
 	);
 };
 
